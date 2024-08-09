5f0a406e50a2c30d8b86e42b2517100aee4f5d9f
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-08-09 21:38:06 +0000

    Update

---
+++ my_project/src/index.js
@@ -0,0 +1,10 @@
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>Welcome to the Financial Business App!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
